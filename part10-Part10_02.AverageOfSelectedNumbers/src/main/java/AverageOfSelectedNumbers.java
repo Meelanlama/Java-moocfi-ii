3873e35ca3a717e9fd56b9429a4f6512796bb37c
Milan <lamameelan32@gmail.com>
2024-07-27 23:42:42 +0545

    10.1 - Handling collections as streams

--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -6,7 +6,38 @@ public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> inputs = new ArrayList<>();
         // toteuta ohjelmasi tänne
+        while (true) {
+            String row = scanner.nextLine();
+            if (row.equals("end")) {
+                break;
+            }
+            inputs.add(row);
+        }
+        double average = inputs.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .average()
+                .getAsDouble();
 
+        double positiveAverage = inputs.stream()
+                .mapToInt(p -> Integer.valueOf(p))
+                .filter(number -> number >= 0)
+                .average()
+                .getAsDouble();
+
+        double negativeAverage = inputs.stream()
+                .mapToInt(n -> Integer.valueOf(n))
+                .filter(number -> number < 0)
+                .average()
+                .getAsDouble();
+
+        String userInput = scanner.nextLine();
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        if (userInput.equals("n")) {
+            System.out.println("Average of the negative numbers: " + negativeAverage);
+        } else {
+            System.out.println("Average of the positive numbers: " + positiveAverage);
+        }
     }
 }
--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+import static java.util.Collections.list;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -8,7 +10,22 @@ public class PositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
-
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(-8);
+        numbers.add(-11);
+        numbers.add(-3);
+        numbers.add(1);
+        numbers.add(8);
+        numbers.add(1);
+//        System.out.println(numbers);
+        positive(numbers);
     }
 
+    public static List<Integer> positive(List<Integer> list) {
+        ArrayList<Integer> values = list.stream()
+                .filter(value -> value > 0)
+                .collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(values);
+        return values;
+    }
 }
