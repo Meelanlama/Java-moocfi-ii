973fb1beccf1b2d1db41d4b78f4c94b0c6c052d4
Milan <lamameelan32@gmail.com>
2024-08-02 20:54:34 +0545

    10.2 - Comparable Inteface exercises

--- part10-Part10_11.WageOrder/src/main/java/Human.java
+++ part10-Part10_11.WageOrder/src/main/java/Human.java
@@ -1,5 +1,5 @@
 
-public class Human {
+public class Human implements Comparable<Human> {
 
     private int wage;
     private String name;
@@ -17,9 +17,14 @@ public class Human {
         return wage;
     }
 
-
     @Override
     public String toString() {
         return name + " " + wage;
+    }
+
+    @Override
+    public int compareTo(Human compared) {
+        //sorting in descending order of wage
+        return Integer.compare(compared.getWage(), this.wage);
     }
 }
