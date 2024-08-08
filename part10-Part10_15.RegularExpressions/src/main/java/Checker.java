7411ed5eae8b6a2752b77f7f63468535b6481c21
Milan <lamameelan32@gmail.com>
2024-08-08 23:45:26 +0545

    Part 10.3 exercise

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,30 @@
 
+import java.util.Scanner;
 
 public class Checker {
 
+    public boolean isDayOfWeek(String string) {
+        if (string.matches("mon|tue|wed|thu|fri|sat|sun")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean allVowels(String string) {
+        //Using + quantifier, vowels must appear one time and can be repeated multiple time.
+        if (string.matches("[aeiouAEIOU]+")) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean timeOfDay(String string) {
+        //00:00:00 and 23:59:59 checking hours
+        //first for checking hour if its start from 01 or 2, checking min,checking sec
+        //"() in hour to check if any format 01-2 is correct"
+        if (string.matches("([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]")) {
+            return true;
+        }
+        return false;
+    }
 }
--- part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -1,9 +1,34 @@
 
+import java.util.Scanner;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+        Scanner scan = new Scanner(System.in);
+        Checker test = new Checker();
+
+        while (true) {
+            System.out.println("Enter a String:");
+//            Calling method and passing parameter from input to check.
+            if (test.isDayOfWeek(scan.nextLine())) {
+                System.out.println("The form is correct.");
+            } else {
+                System.out.println("The form is incorrect.");
+            }
+
+            if (test.allVowels(scan.nextLine())) {
+                System.out.println("The form is correct.");
+            } else {
+                System.out.println("The form is incorrect.");
+            }
+
+            if (test.timeOfDay(scan.nextLine())) {
+                System.out.println("The form is correct.");
+            } else {
+                System.out.println("The form is incorrect.");
+            }
+        }
     }
 }
