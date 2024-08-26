d36dd58c4195d5188908f590b6aa277b7694d578
Milan <lamameelan32@gmail.com>
2024-08-26 18:54:38 +0545

    Completed 12.6 exercise

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
+        Random rdm = new Random();
         Scanner scanner = new Scanner(System.in);
-    }
+        System.out.println("How many random numbers should be printed?");
+        int num = scanner.nextInt();
 
+        for (int i = 0; i < num; i++) {
+            int randomNum = rdm.nextInt(11);
+            System.out.println(randomNum);
+        }
+    }
 }
