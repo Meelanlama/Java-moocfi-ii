b110843be90f3298a6d413064ec186982aec7829
Milan <lamameelan32@gmail.com>
2024-08-26 21:13:56 +0545

    Completed 12.3- Randomness

--- part12-Part12_08.Lottery/src/main/java/LotteryRow.java
+++ part12-Part12_08.Lottery/src/main/java/LotteryRow.java
@@ -8,6 +8,7 @@ public class LotteryRow {
 
     public LotteryRow() {
         // Draw the numbers when the LotteryRow is created
+        this.numbers = new ArrayList<>();
         this.randomizeNumbers();
     }
 
@@ -19,12 +20,29 @@ public class LotteryRow {
         // Initialize the list for numbers
         this.numbers = new ArrayList<>();
         // Implement the random number generation here
+        Random randomNum = new Random();
         // the method containsNumber is probably useful
+        //generating 7 random numbers which is not repeated
+        while (this.numbers.size() < 7) {
+            int num = randomNum.nextInt(40) + 1;
+            if (!containsNumber(num)) {
+                this.numbers.add(num);
+            }
+        }
+//        for (int i = 0; i < 7; i++) {
+//        int num = randomNum.nextInt(40) + 1;
+//        if (!containsNumber(num)) {
+//            this.numbers.add(num);
+//        } else {
+//            i--; // Decrement i to repeat the iteration if a duplicate is found
+//        }
     }
 
     public boolean containsNumber(int number) {
         // Check here whether the number is among the drawn numbers
+        if (this.numbers.contains(number)) {
+            return true;
+        }
         return false;
     }
 }
-
